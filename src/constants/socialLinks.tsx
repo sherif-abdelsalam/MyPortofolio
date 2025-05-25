96d8b001da28f7f192e1ac308cd50796e1766278
Sherif Abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-05-25 12:27:22 +0300

    update resume link

--- src/constants/socialLinks.tsx
+++ src/constants/socialLinks.tsx
@@ -1,13 +1,10 @@
-import { Phone } from "lucide-react";
-
 const myLinks = {
   github: "https://github.com/sherif-abdelsalam",
   linkedin: "https://www.linkedin.com/in/sherif-abdelsalam/",
   email: "mailto:sherif.abdelsalam.111@gmail.com",
   phone: "+20 109 5289 206",
   location: "Egypt",
-  resume:
-    "https://drive.google.com/file/d/1jSljvu6rOTLx6MLx6nZsoTpWF_Y7fO-B/view?usp=sharing",
+  resume: "https://www.1resume.app/r/sherif-hodhod-resume",
 };
 
 export default myLinks;
